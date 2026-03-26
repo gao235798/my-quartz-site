9deabfc22dbcd0f03dbcadbf09f6854a63b1fc67
gao235798 <46990840+gao235798@users.noreply.github.com>
2026-03-26 16:31:04 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -78,6 +78,7 @@ const config: QuartzConfig = {
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
+      Plugin.PageList(),  
       Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
